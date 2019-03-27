1ee014ff1675aec442104087a1acbbaf7f4fe1c0
Fletcher Chapin <fletchapin@gmail.com>
2019-03-27 10:21:22 -0700

    Got table updates working

--- epl_mc.py
+++ epl_mc.py
@@ -9,9 +9,8 @@ goals_df = pd.DataFrame(columns=['Club', 'Avg GF Home', 'Avg GF Away',
                                  'Avg GA Home', 'Avg GA Away'])
 
 
-# parses the score and returns goals for and against
-def score_parser(index, awayFlag, df=season_csv):
-    score = df['Result'][index]
+# returns the string score passed in as two integers
+def parse_score(score, awayFlag=False):
     scoreArr = score.split("-")
     if awayFlag:
         goalsFor = int(scoreArr[1])
@@ -21,6 +20,55 @@ def score_parser(index, awayFlag, df=season_csv):
         goalsAgainst = int(scoreArr[1])
     return goalsFor, goalsAgainst
 
+
+# gets the score at the given index and returns goals for and against
+def get_score(index, awayFlag, df=season_csv):
+    score = df['Result'][index]
+    return parse_score(score, awayFlag)
+
+
+# returns the number of points for each team based on score
+def get_points(score):
+    home_g, away_g = parse_score(score)
+    if home_g > away_g:
+        return 3, 0
+    elif home_g == away_g:
+        return 1, 1
+    else:
+        return 0, 3
+
+
+# updates league table df based on stats passed in
+def update_table(club, points, gf, ga, awayFlag, df=table_csv):
+    view = df[df['Club'] == club]
+
+    # update matches played
+    df.loc[df['Club'] == club, 'Matches'] = view['Matches'] + 1
+    if awayFlag:
+        df.loc[df['Club'] == club, 'Away Matches'] = view['Away Matches'] + 1
+    else:
+        df.loc[df['Club'] == club, 'Home Matches'] = view['Home Matches'] + 1
+
+    # update points and goals
+    if points == 3:
+        df.loc[df['Club'] == club, 'Wins'] = view['Wins'] + 1
+    elif points == 1:
+        df.loc[df['Club'] == club, 'Draws'] = view['Draws'] + 1
+    else:
+        df.loc[df['Club'] == club, 'Losses'] = view['Losses'] + 1
+
+    df.loc[df['Club'] == club, 'Pts'] = view['Pts'] + points
+    df.loc[df['Club'] == club, 'GF'] = view['GF'] + gf
+    df.loc[df['Club'] == club, 'GA'] = view['GA'] + ga
+    df.loc[df['Club'] == club, 'GD'] = df.loc[df['Club'] == club, 'GF'] - df.loc[df['Club'] == club, 'GA']
+
+
+def sort_table(df=table_csv):
+    df = df.sort_values(['Pts', 'GD'], 0, False)
+    for i in range(df['Position'].count()):
+        df['Position'].iloc[i] = i+1
+
+    return df
 
 # get avg home/away goals for/against for each team
 played_matches = season_csv[season_csv['Result'].notna()]
@@ -34,14 +82,14 @@ for team in table_csv['Club']:
     while i < num_rows:
         if team == season_csv['Home Team'][i]:
             try:
-                gf, ga = score_parser(i, False, played_matches)
+                gf, ga = get_score(i, False, played_matches)
                 gf_home = gf_home + gf
                 ga_home = ga_home + ga
             except:
                 break
         elif team == season_csv['Away Team'][i]:
             try:
-                gf, ga = score_parser(i, True, played_matches)
+                gf, ga = get_score(i, True, played_matches)
                 gf_away = gf_away + gf
                 ga_away = ga_away + ga
             except:
@@ -86,12 +134,22 @@ for i in range(remaining_matches['Date'].count()):
                               'Poisson HG': poi_home, 'Poisson AG': poi_away},
                              ignore_index=True)
 
-    score = str(int(np.round(poi_home))) + ' - ' + str(int(np.round(poi_away)))
+    rd_poi_home = int(np.round(poi_home))
+    rd_poi_away = int(np.round(poi_away))
+    score = str(rd_poi_home) + ' - ' + str(rd_poi_away)
     remaining_matches['Result'].iloc[i] = score
 
-remaining_matches.to_csv('epl-results-1819')
+    home_pts, away_pts = get_points(score)
 
-# TODO: update table based on these results
+    update_table(home_team, home_pts, rd_poi_home, rd_poi_away, False)
+    update_table(away_team, away_pts, rd_poi_away, rd_poi_home, True)
+
+    file_path = './Results/epl-table' + str(i) + '.csv'
+    table_csv = sort_table()
+    table_csv.to_csv(file_path)
+
+table_csv.to_csv('./Results/epl-table-final.csv')
+remaining_matches.to_csv('./Results/epl-results-1819.csv')
 
 # references:
 # https://medium.com/@adamfreymiller/a-monte-carlo-simulation-of-the-2017-18-premier-league-season-3b7bbe8b8a13
