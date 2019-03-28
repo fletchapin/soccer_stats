bc7cbfd4304fa09f10ad83ca5be92e972df8da2e
Fletcher Chapin <fletchapin@gmail.com>
2019-03-27 23:09:57 -0700

    Got Poisson simulations working

--- epl_mc.py
+++ epl_mc.py
@@ -1,13 +1,13 @@
+import os
 import pandas as pd
 import plotly as plt
 import numpy as np
 import numpy.random as rd
 
 season_csv = pd.read_csv('epl-2018.csv')
-table_csv = pd.read_csv('epl-table.csv')
+table_csv = pd.read_ctable_csv = pd.read_csv('epl-table.csv')
 goals_df = pd.DataFrame(columns=['Club', 'Avg GF Home', 'Avg GF Away',
                                  'Avg GA Home', 'Avg GA Away'])
-
 
 # returns the string score passed in as two integers
 def parse_score(score, awayFlag=False):
@@ -111,6 +111,8 @@ for team in table_csv['Club']:
 # get list of matches remaining which need to be predicted
 calc_df = pd.DataFrame(columns=['Home Team', 'Away Team', 'Ex HG', 'Ex AG'])
 remaining_matches = season_csv[season_csv['Result'].isna()]
+poisson = {}
+poi_count = 100
 for i in range(remaining_matches['Date'].count()):
     home_team = remaining_matches['Home Team'].iloc[i]
     away_team = remaining_matches['Away Team'].iloc[i]
@@ -126,30 +128,62 @@ for i in range(remaining_matches['Date'].count()):
     exg_away = (ex_agf + ex_hga) / 2
 
     # run poisson 1000 times per match, round avg score to nearest whole number
-    poi_home = np.mean(rd.poisson(exg_home, 1000))
-    poi_away = np.mean(rd.poisson(exg_away, 1000))
+    poi_home = rd.poisson(exg_home, poi_count)
+    poi_away = rd.poisson(exg_away, poi_count)
+
+    strA = (str(i) + 'a')
+    strB = (str(i) + 'b')
+    poisson[strA] = poi_home
+    poisson[strB] = poi_away
 
     calc_df = calc_df.append({'Home Team': home_team, 'Away Team': away_team,
-                              'Ex HG': exg_home, 'Ex AG': exg_away,
-                              'Poisson HG': poi_home, 'Poisson AG': poi_away},
+                              'Ex HG': exg_home, 'Ex AG': exg_away},
                              ignore_index=True)
+for j in range(poi_count):
+    table_csv = pd.read_csv('epl-table.csv')
+    remaining_matches = season_csv[season_csv['Result'].isna()]
+    for i in range(remaining_matches['Date'].count()):
+        home_team = remaining_matches['Home Team'].iloc[i]
+        away_team = remaining_matches['Away Team'].iloc[i]
 
-    rd_poi_home = int(np.round(poi_home))
-    rd_poi_away = int(np.round(poi_away))
-    score = str(rd_poi_home) + ' - ' + str(rd_poi_away)
-    remaining_matches['Result'].iloc[i] = score
+        strA = (str(i) + 'a')
+        strB = (str(i) + 'b')
 
-    home_pts, away_pts = get_points(score)
+        rd_poi_home = poisson[strA][j]
+        rd_poi_away = poisson[strB][j]
+        score = str(rd_poi_home) + ' - ' + str(rd_poi_away)
+        remaining_matches['Result'].iloc[i] = score
 
-    update_table(home_team, home_pts, rd_poi_home, rd_poi_away, False)
-    update_table(away_team, away_pts, rd_poi_away, rd_poi_home, True)
+        home_pts, away_pts = get_points(score)
 
-    file_path = './Results/epl-table' + str(i) + '.csv'
-    table_csv = sort_table()
-    table_csv.to_csv(file_path)
+        update_table(home_team, home_pts, rd_poi_home, rd_poi_away, False, table_csv)
+        update_table(away_team, away_pts, rd_poi_away, rd_poi_home, True, table_csv)
 
-table_csv.to_csv('./Results/epl-table-final.csv')
-remaining_matches.to_csv('./Results/epl-results-1819.csv')
+        file_path = './Results/' + str(j) + '/epl-table' + str(i) + '.csv'
+        table_csv = sort_table(table_csv)
+        try:
+            table_csv.to_csv(file_path)
+        except FileNotFoundError:
+            path = os.getcwd()
+            head, tail = os.path.split(file_path)
+            os.mkdir(path + '/' + head)
+            table_csv.to_csv(file_path)
+
+    table_csv.to_csv('./Results/' + str(j) + '/epl-table-final.csv')
+    remaining_matches.to_csv('./Results/' + str(j) + '/epl-results-1819.csv')
+    table_csv.iloc[0:0]
+
+winners = {}
+for j in range(poi_count):
+    filepath = './Results/' + str(j) + '/epl-table-final.csv'
+    table_csv = pd.read_csv(filepath)
+    winner = table_csv[table_csv['Position'] == 1]['Club']
+    if winner in winnners:
+        winners[winner] += 1
+    else:
+        winners[winner] = 1
+
+print(winners)
 
 # references:
 # https://medium.com/@adamfreymiller/a-monte-carlo-simulation-of-the-2017-18-premier-league-season-3b7bbe8b8a13
