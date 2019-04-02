4dd390f77a30200a4605b3e55a76488e195df1f3
Fletcher Chapin <fletchapin@gmail.com>
2019-04-02 10:28:12 -0700

    Fixed aggregate table

--- epl_analysis.py
+++ epl_analysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import plotly as plt
+import matplotlib.pyplot as plt
 from tabulate import tabulate
 
 
@@ -10,24 +10,23 @@ def add_to_dict(dict, entry):
         dict[entry] = 1
 
 
-def agg_table(orig_df, add_df):
-    #for team in orig_df['Club']:
-        #team_stats = add_df[add_df['Club'] == team]
-    orig_df['Position'] += add_df['Position']
-    orig_df['Wins'] += add_df['Wins']
-    orig_df['Draws'] += add_df['Draws']
-    orig_df['Losses'] += add_df['Losses']
-    orig_df['GA'] += add_df['GA']
-    orig_df['GF'] += add_df['GF']
-    orig_df['GD'] += add_df['GD']
-    orig_df['Pts'] += add_df['Pts']
+def agg_table(orig_df, add_df, j):
+    add_df = add_df.sort_values('Unnamed: 0', 0, True)
+    add_df = add_df.drop(add_df.columns[0], axis=1)
+
+    orig_df['Position'] = orig_df['Position'].values + add_df['Position'].values
+    orig_df['Wins'] = orig_df['Wins'].values + add_df['Wins'].values
+    orig_df['Draws'] = orig_df['Draws'].values  + add_df['Draws'].values
+    orig_df['Losses'] = orig_df['Losses'].values + add_df['Losses'].values
+    orig_df['GA'] = orig_df['GA'].values + add_df['GA'].values
+    orig_df['GF'] = orig_df['GF'].values + add_df['GF'].values
+    orig_df['GD'] = orig_df['GD'].values + add_df['GD'].values
+    orig_df['Pts'] = orig_df['Pts'].values + add_df['Pts'].values
 
     return orig_df
 
 
 def avg_table(orig_df, count):
-    #for team in orig_df['Club']:
-        #team_stats = add_df[add_df['Club'] == team]
     orig_df['Position'] = orig_df['Position'] / count
     orig_df['Wins'] = orig_df['Wins'] / count
     orig_df['Draws'] = orig_df['Draws'] / count
@@ -40,32 +39,32 @@ def avg_table(orig_df, count):
     return orig_df
 
 
+pos_dict = {}
+for team in pd.read_csv('epl-table.csv')['Club']:
+    pos_dict[team] = {}
+
 poi_count = 100
 winners = {}
-pos_dict = {}
 for j in range(poi_count):
     filepath = './Results/' + str(j) + '/epl-table-final.csv'
     table_csv = pd.read_csv(filepath)
-    table_csv.drop(table_csv.columns[0], axis=1)
     winner = table_csv.loc[table_csv['Position'] == 1, 'Club'].values[0]
     add_to_dict(winners, winner)
 
     if j == 0:
         table_df = pd.read_csv(filepath)
+        table_df = table_df.sort_values('Unnamed: 0', 0, True)
         table_df = table_df.drop(table_df.columns[0], axis=1)
     else:
-        table_df = agg_table(table_df, table_csv)
+        table_df = agg_table(table_df, table_csv, j)
 
     for team in table_csv['Club']:
-        if j == 0:
-            pos_dict[team] = {}
         pos = table_csv.loc[table_csv['Club'] == team, 'Position'].values[0]
         add_to_dict(pos_dict[team], pos)
 
 table_df = avg_table(table_df, poi_count)
 table_df.to_csv('./Results/epl-table-agg.csv')
 
-# add in percentages, and convert NaN to ''
 pos_df = pd.DataFrame(pos_dict).T
 pos_df = pos_df.fillna('')
 pos_df.to_csv('./Results/epl-pos-percent.csv')
